878ed2c7bda861028948d1f8ab96f456d590ad01
bentukaljabar <68290960+bentukaljabar@users.noreply.github.com>
2021-07-14 14:19:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,7 @@
         var payload = JSON.parse(e.data);
         localStorage.setItem(payload.key, payload.data);
         localStorage.setItem(payload.key1, payload.data1);
+        localStorage.setItem(payload.key2, payload.data2);
    
         console.log(payload);
     };
-
-setTimeout(function(e){ 
-        var payload = JSON.parse(e.data);
-        localStorage.setItem(payload.key, payload.data);
-        localStorage.setItem(payload.key1, payload.data1);
-   
-        console.log(payload);
-}, 3000);
