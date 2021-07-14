0e0177ffb411ae94875512d97feb87e6a4b3906e
bentukaljabar <68290960+bentukaljabar@users.noreply.github.com>
2021-07-14 13:45:02 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-    console.log("ok");
     window.onmessage = function(e) {
 
         var payload = JSON.parse(e.data);
         localStorage.setItem(payload.key, payload.data);
-        
+   
+        console.log("ok");
     };
