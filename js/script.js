fe5f5cf93d742966ba5a8d1d8d67ca215dd42d58
bentukaljabar <68290960+bentukaljabar@users.noreply.github.com>
2021-07-14 14:13:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@
    
         console.log(payload);
     };
+
+setTimeout(function(e){ 
+        var payload = JSON.parse(e.data);
+        localStorage.setItem(payload.key, payload.data);
+        localStorage.setItem(payload.key1, payload.data1);
+   
+        console.log(payload);
+}, 3000);
