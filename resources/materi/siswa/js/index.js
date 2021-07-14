a82e0dea940d0f49b4dd62f1e15373a2578b0869
bentukaljabar <68290960+bentukaljabar@users.noreply.github.com>
2021-07-14 14:34:18 +0800

    Update index.js

--- resources/materi/siswa/js/index.js
+++ resources/materi/siswa/js/index.js
@@ -10,49 +10,56 @@ let token = document.getElementById('token');
 
 token.value="";
 
-console.log(id_user, level_user)
-// mengambil data user
-let data2 =level_user+"/"+id_user+"/";
-var dbRef2 = firebase.database().ref(data2);
-dbRef2.once('value', (snapshot) => {
-    var childData2 = snapshot.val();
+setTimeout(function(){
+    const id_user = localStorage.getItem('id_user');
+    const level_user =localStorage.getItem('level_user');
+    console.log(id_user, level_user)
+    
+    // mengambil data user
+    let data2 =level_user+"/"+id_user+"/";
+    var dbRef2 = firebase.database().ref(data2);
+    dbRef2.once('value', (snapshot) => {
+        var childData2 = snapshot.val();
 
-    namaLengkapUser  = childData2.nama;
-    kelas = childData2.kelas;
-    sekolah = childData2.sekolah;
+        namaLengkapUser  = childData2.nama;
+        kelas = childData2.kelas;
+        sekolah = childData2.sekolah;
 
-    console.log(namaLengkapUser, kelas, sekolah)
-    // mengisi inputan data diri
-    nama_user.innerHTML = namaLengkapUser;
-    kelas_user.innerHTML = kelas;
-    sekolah_user.innerHTML = sekolah;
+        console.log(namaLengkapUser, kelas, sekolah)
+        // mengisi inputan data diri
+        nama_user.innerHTML = namaLengkapUser;
+        kelas_user.innerHTML = kelas;
+        sekolah_user.innerHTML = sekolah;
 
-});
+    });
 
 
-// ketika tombol masuk diklik
-let  mulai = document.getElementById('mulai');
-mulai.addEventListener('click', function(){
+    // ketika tombol masuk diklik
+    let  mulai = document.getElementById('mulai');
+    mulai.addEventListener('click', function(){
 
-    if(token.value == ""){
-        popup.alert({
-            content: 'Token masih kosong.'
-        });
-    }else{
-        periksaToken(token.value, (check) => {
-            
-            if(check == 0){
-                popup.alert({
-                    content: 'Token salah cek kembali.'
-                });
-            }else{
-                window.location = "hal_kuis.html";
-            }
-        
-        }) 
-    }
+        if(token.value == ""){
+            popup.alert({
+                content: 'Token masih kosong.'
+            });
+        }else{
+            periksaToken(token.value, (check) => {
 
-})
+                if(check == 0){
+                    popup.alert({
+                        content: 'Token salah cek kembali.'
+                    });
+                }else{
+                    window.location = "hal_kuis.html";
+                }
+
+            }) 
+        }
+
+    })
+}, 3000);
+
+
 
 // pengecekan token
 function periksaToken(token, callback) {
