36cac5be4fcd674aa7e4f0fdbf2e60850da33d83
bentukaljabar <68290960+bentukaljabar@users.noreply.github.com>
2021-07-23 06:57:27 +0800

    Update index.js

--- resources/materi/siswa/js/index.js
+++ resources/materi/siswa/js/index.js
@@ -5,6 +5,7 @@ const level_user =localStorage.getItem('level_user');
 let nama_user = document.getElementById('nama_user');
 let sekolah_user = document.getElementById('sekolah_user');
 let kelas_user = document.getElementById('kelas_user');
+let bgkanan = document.querySelector('.data_diri');
 
 let token = document.getElementById('token');
 
@@ -14,6 +15,14 @@ setTimeout(function(){
     const id_user = localStorage.getItem('id_user');
     const level_user =localStorage.getItem('level_user');
     console.log(id_user, level_user)
+    
+    if(id_user == undefined){
+        bgkanan.innerHTML =`
+        <center>
+            <h1>Anda halur login atau bergabung kelas terlebih dahulu.</h1>
+        </center>
+        `
+    }
     
     // mengambil data user
     let data2 =level_user+"/"+id_user+"/";
